b0ffd75e7917ef98fcf6190115b99f2f477c2f03
Irina <vinogradova8998@gmail.com>
2024-11-22 22:13:10 +0200

    first commit

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import NavItem from "./NavItem.tsx";
+import {navItems} from "../utils/constants.ts";
+
+
+const Navigation = () => {
+    return (
+        <nav className="fixed top-1  left-10">
+            <ul className="flex space-x-4">
+                {navItems.map(item => <NavItem  itemTitle={item} key={item}/>)}
+            </ul>
+        </nav>
+    );
+};
+
+
+export default Navigation;
